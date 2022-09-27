f5994dd739d2a7e75616a9f1724efe48631f845d
Madswolf <mads@madswolf.dk>
2022-09-28 01:27:24 +0200

    minor refinements

--- test_wasm/src/main.rs
+++ test_wasm/src/main.rs
@@ -10,15 +10,20 @@ struct Model {
 
 fn roll(state:&UseStateHandle<Model>, sound:&UseMediaHandle, input_ref:&NodeRef){
     let input = input_ref.cast::<HtmlInputElement>().unwrap();
+
     let mut state_value = state.value;
     let mut state_intensity = state.intensity;
     let value = input.value().parse::<i64>();
 
     if let Ok(value) = value {
         if value != state_value  {
-            state_value = value;
+            state_value = num::clamp(value, 1, std::i64::MAX);
             state_intensity = 1;
         }
+    }
+
+    if state_value == 1 {
+        return
     }
 
     let num = rand::thread_rng().gen::<f64>();
@@ -41,14 +46,15 @@ fn roll(state:&UseStateHandle<Model>, sound:&UseMediaHandle, input_ref:&NodeRef)
 #[function_component(App)]
 fn app() -> Html {
     let state = use_state(|| Model {
-        value: 0,
-        intensity: 0
+        value: 69,
+        intensity: 1
     });
 
-    let node_video = use_node_ref();
+    let node_audio = use_node_ref();
     let src = dotenv_codegen::dotenv!("LOSS_SOUND_EFFECT_URL");
-    let sound = use_media(node_video.clone(), src.to_owned());
+    let sound = use_media(node_audio.clone(), src.to_owned());
     let input_ref = use_node_ref();
+
     let onclick = {
         let state = state.clone();
         let sound = sound.clone();
@@ -69,13 +75,14 @@ fn app() -> Html {
         })
     };
 
+
     html!{
         <body class={if state.value == 1 {classes!("background")} else {classes!()}}>
             <input ref={input_ref} onkeydown={keydown} value={state.value.to_string()}/>
             <div class="flexbox" onclick={onclick}>
-                <audio ref={node_video}/>
+                <audio ref={node_audio}/>
                 <p class={if state.value == 1 {classes!("result","one")} else {classes!("result")}} style={format!("color:red;font-size: {:?}px;",20 * state.intensity)}>{state.value}</p>
-            </div>
+            </div>  
         </body>
     }
 }
