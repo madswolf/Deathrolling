b71757b28b704c08a82d2d51f5d1c90cd5ce745a
oeje <MadsWJespersen@users.noreply.github.com>
2022-09-27 12:43:38 +0200

    cleaned code by using macro and references

--- test_wasm/src/main.rs
+++ test_wasm/src/main.rs
@@ -2,6 +2,7 @@ use yew::prelude::*;
 use web_sys::{HtmlInputElement, EventTarget};
 use wasm_bindgen::JsCast;
 use rand::Rng;
+use yew_hooks::*;
 
 struct Model {
     value: i64,
@@ -13,6 +14,41 @@ fn to_html_input_element(target:Option<EventTarget>) -> HtmlInputElement{
     target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok()).unwrap()
 }
 
+#[macro_export]
+macro_rules!  rollMacro  {
+    ($state:expr, $sound: expr, $input_ref:expr) => {
+        let input = $input_ref.cast::<HtmlInputElement>().unwrap();
+        let mut state_value = $state.value;
+        let mut state_intensity = $state.intensity;
+        let value = input.value().parse::<i64>();
+
+        if let Ok(value) = value {
+            if value != state_value  {
+                state_value = value;
+                state_intensity = 1;
+            }
+        }
+
+        let num = rand::thread_rng().gen::<f64>();
+        let roll = (num * state_value as f64).ceil() as i64;
+        
+        let state_value = roll;
+        if roll == 1 {
+            $sound.play();
+            state_intensity = 1;
+            log::info!("donorono");
+        } else {
+            state_intensity = state_intensity +1;
+            log::info!("gaming intensity: {:?} ", state_intensity);
+        }
+    
+        $state.set(Model {
+            value: state_value,
+            intensity: state_intensity
+        });
+    };
+}
+
 #[function_component(App)]
 fn app() -> Html {
     let state = use_state(|| Model {
@@ -22,101 +58,38 @@ fn app() -> Html {
 
     let node_video = use_node_ref();
     let src = "https://media.clown.mads.monster/bass2.mp3";
-    let sound = yew_hooks::use_media(node_video.clone(), src.to_owned());
+    let sound = use_media(node_video.clone(), src.to_owned());
 
-    fn roll(state: UseStateHandle<Model>) -> (){
-        let num = rand::thread_rng().gen::<f64>();
-        let roll = (num * state.value as f64).ceil() as i64;
-        
-        let state_value = roll;
-        let mut state_intensity = state.intensity;
-        if roll == 1 {
-            log::info!("donorono");
-        } else {
-            state_intensity = state_intensity +1;
-            log::info!("gaming intensity: {:?} ", state_intensity);
-        }
+    let input_ref = use_node_ref();
 
-        state.set(Model {
-            value: state_value,
-            intensity: state_intensity
-        });
-    }
 
     let onclick = {
         let state = state.clone();
         let sound = sound.clone();
+        let input_ref = input_ref.clone();
         Callback::from(move |event:MouseEvent| {
-            let num = rand::thread_rng().gen::<f64>();
-            let roll = (num * state.value as f64).ceil() as i64;
-            
-            let state_value = roll;
-            let mut state_intensity = state.intensity;
-            if roll == 1 {
-                sound.play();
-                log::info!("donorono");
-            } else {
-                state_intensity = state_intensity +1;
-                log::info!("gaming intensity: {:?} ", state_intensity);
-            }
-    
-    
-            log::info!("set value to {:?}",state_value);
-            
-            state.set(Model {
-                value: state_value,
-                intensity: state_intensity
-            });
+            rollMacro!(state, sound, input_ref);
         })
     };
 
-    let keydown ={
+    let keydown = {
         let state = state.clone();
         let sound = sound.clone();
+        let input_ref = input_ref.clone();
+
         Callback::from(move |event: KeyboardEvent| {
             if event.key() == "Enter" {
-
-                let input = to_html_input_element(event.target());
-
-
-                let mut state_value = state.value;
-                let mut state_intensity = state.intensity;
-                let value = input.value().parse::<i64>();
-
-                if let Ok(value) = value {
-                    if value != state_value  {
-                        state_value = value;
-                        state_intensity = 1;
-                    }
-                }
-
-                let num = rand::thread_rng().gen::<f64>();
-                let roll = (num * state_value as f64).ceil() as i64;
-                
-                let state_value = roll;
-                if roll == 1 {
-                    sound.play();
-                    state_intensity = 1;
-                    log::info!("donorono");
-                } else {
-                    state_intensity = state_intensity +1;
-                    log::info!("gaming intensity: {:?} ", state_intensity);
-                }
-            
-                state.set(Model {
-                    value: state_value,
-                    intensity: state_intensity
-                });
-                    }
+                rollMacro!(state, sound, input_ref);
+            }
         })
     };
 
     html!{
         <body class={if state.value == 1 {classes!("background")} else {classes!()}}>
-            <input onkeydown={keydown} value={state.value.to_string()}/>
+            <input ref={input_ref} onkeydown={keydown} value={state.value.to_string()}/>
             <div class="flexbox" onclick={onclick}>
-            <audio ref={node_video}/>
-            <p class={if state.value == 1 {classes!("result","one")} else {classes!("result")}} style={format!("color:red;font-size: {:?}px;",20 * state.intensity)}>{state.value}</p>
+                <audio ref={node_video}/>
+                <p class={if state.value == 1 {classes!("result","one")} else {classes!("result")}} style={format!("color:red;font-size: {:?}px;",20 * state.intensity)}>{state.value}</p>
             </div>
         </body>
     }
