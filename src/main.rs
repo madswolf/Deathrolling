91f412f835866e65697c05442286485757d1401c
oeje <MadsWJespersen@users.noreply.github.com>
2022-09-30 20:59:06 +0200

    added background and result color randomization

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use yew::prelude::*;
 use web_sys::{HtmlInputElement};
-use rand::Rng;
+use rand::{Rng, seq::SliceRandom};
 use yew_hooks::*;
 use std::env;
 
+#[derive(Eq, PartialEq, Clone)]
+struct Colors {
+    background: String,
+    result: String
+}
+
 struct Model {
     value: i64,
-    intensity: i64
+    intensity: i64,
+    colors: Colors
 }
+
 
 fn roll(state:&UseStateHandle<Model>, sound:&UseMediaHandle, input_ref:&NodeRef){
     let input = input_ref.cast::<HtmlInputElement>().unwrap();
+
+    let backgrounds:Vec<Colors> = vec![
+        Colors{background:"#52AA5E".to_owned(),result:"#007991".to_owned()},
+        Colors{background:"#F3DE8A".to_owned(),result:"#2A2B2A".to_owned()},
+        Colors{background:"#EB9486".to_owned(),result:"#233D4D".to_owned()},
+        Colors{background:"#5299D3".to_owned(),result:"#F2DFD7".to_owned()},
+        Colors{background:"#FEE1C7".to_owned(),result:"#684E32".to_owned()}
+    ];
 
     let mut state_value = state.value;
     let mut state_intensity = state.intensity;
@@ -37,10 +53,19 @@ fn roll(state:&UseStateHandle<Model>, sound:&UseMediaHandle, input_ref:&NodeRef)
     } else {
         state_intensity = state_intensity + 1;
     }
+    
+    let thing = backgrounds.iter().cloned().filter(|x|  {x.clone() != state.colors}).collect::<Vec<_>>();
+    let Colors{background:mut background_color, result: result_color} = thing.choose(&mut rand::thread_rng()).unwrap().to_owned();
+    log::info!("background_color:{} result_color:{}", background_color, result_color);
+
+    if state_value == 31 {
+        background_color = "#5d198a".to_owned();
+    }
 
     state.set(Model {
         value: state_value,
-        intensity: state_intensity
+        intensity: state_intensity,
+        colors:Colors{background:background_color, result: result_color}
     });
 }
 
@@ -48,7 +73,8 @@ fn roll(state:&UseStateHandle<Model>, sound:&UseMediaHandle, input_ref:&NodeRef)
 fn app() -> Html {
     let state = use_state(|| Model {
         value: 69,
-        intensity: 1
+        intensity: 1,
+        colors:Colors{background:"#52AA5E".to_owned(), result:"red".to_owned()}
     });
 
     let node_audio = use_node_ref();
@@ -78,11 +104,11 @@ fn app() -> Html {
 
 
     html!{
-        <body class={if state.value == 1 {classes!("background")} else {classes!()}}>
+        <body style={format!("background-color: {};", state.colors.background)} class={if state.value == 1 {classes!("background")} else {classes!()}}>
             <input ref={input_ref} onkeydown={keydown} value={state.value.to_string()}/>
             <div class="flexbox" onclick={onclick}>
                 <audio ref={node_audio} preload="auto"/>
-                <p class={if state.value == 1 {classes!("result","one")} else {classes!("result")}} style={format!("color:red;font-size: {:?}px;",20 * state.intensity)}>{state.value}</p>
+                <p class={if state.value == 1 {classes!("result","one")} else {classes!("result")}} style={format!("color:{};font-size: {:?}px;",state.colors.result, 20 * state.intensity + 30)}>{state.value}</p>
             </div>  
         </body>
     }
